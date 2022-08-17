98e49e895962a31dde4049b0a84a3371ba958530
penta-di-mono <madamea521@gmail.com>
2022-08-17 11:06:14 +0900

    kdkd second

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 		var winHT = $(window).height();
 		var navindex = $(this).index();
 		var nowTop = navindex * winHT
-//		console.log(nowTop);
+		//console.log(nowTop);
 
 		$('html').stop().animate({scrollTop:nowTop},700);
 		$('div.qck_point ul> li').removeClass('on');
@@ -52,6 +52,7 @@ $(document).ready(function(){
 		$('html').stop().animate({'scrollTop':next},1200);
 	});
 
+	//배열,키캡,스위치
 	$('ul.three_things li>a>p').on('mouseover', function(){
 		$(this).removeClass('on');
 		$(this).prev('h4').addClass('on');
@@ -61,6 +62,7 @@ $(document).ready(function(){
 		$(this).next('p').addClass('on');
 	});
 
+	//클릭하면 설명창 나오기
 	$('ul.three_things li').click(function(event){
 		event.preventDefault();
 		var index = $(this).index();
@@ -69,24 +71,27 @@ $(document).ready(function(){
 		$('article#machine section').eq(index).addClass('on').animate({'left':0},1000);
 	});
 
+	//스위치는 내려가기
 	$('ul.three_things li:last-child').click(function(){
 		var next = $(this).parents('article').next().offset().top;
 		$('html').stop().animate({'scrollTop':next},1200);
 	});
 
+	//뒤로가기 버튼
 	$('span.circle_back').click(function(){
 		$(this).parents('section').animate({'left':'-100%'},1000,function(){
 			$('article#machine section.descript').removeClass('on');
 		});
 	});
 
+	//옵션	
 	$('ul.op_list >li.options').click(function(event){
 		event.preventDefault();
 		$('ul.op_list >li.options').removeClass('on');
 		$(this).addClass('on');
 	});
     
-//    스위치 설명
+	//스위치 설명
     $('.switch_sec').click(function(){
 		$('.switch_sec').removeClass('on');
 		$(this).addClass('on');
@@ -96,7 +101,7 @@ $(document).ready(function(){
 	});
     
     
-//    타건영상 썸네일 슬라이드
+	//타건영상 썸네일 슬라이드
     $('.ar_right').click(function(){
         $('div.video_wrap ul.video_list li').eq(0).insertAfter('div.video_wrap ul.video_list li:last-child');
     });
@@ -104,8 +109,8 @@ $(document).ready(function(){
         $('div.video_wrap ul.video_list li').eq(-1).insertBefore('div.video_wrap ul.video_list li:first-child');
     });
     
-//    마지막 체크
-    $('ul.faq_list li').click(function(){
+	//마지막 체크
+    $('ul.faq_list li').click(function(event){
        event.preventDefault();
        $('ul.faq_list li').removeClass('on');
        $(this).addClass('on');
@@ -117,8 +122,8 @@ $(document).ready(function(){
         $('ul.answer_faq li').eq($answerIndex).addClass('on');
     });
     
-//    아웃트로
-    $('ul.big_menu>li a.choice').click(function(){
+	//아웃트로
+    $('ul.big_menu>li a.choice').click(function(event){
         event.preventDefault();
         $(this).addClass('off');
         $(this).next('ul').addClass('on');
@@ -129,7 +134,7 @@ $(document).ready(function(){
         var $this = $(this).index();
         var $quick = $this + 2;
         var winHT = $(window).height();
-//        console.log($this);
+		//console.log($this);
         
         if($this <= 2){
            $('html').stop().animate({scrollTop:winHT * $quick},700);
